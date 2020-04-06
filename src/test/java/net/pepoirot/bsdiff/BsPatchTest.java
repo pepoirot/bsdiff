18ce7f9b466f4d431c4fe4022d7cafb8f28f47fc
Pierre-Etienne Poirot <pepoirot@gmx.com>
2020-04-06 21:21:20 +1000

    Convert to JUnit 5

--- src/test/java/net/pepoirot/bsdiff/BsPatchTest.java
+++ src/test/java/net/pepoirot/bsdiff/BsPatchTest.java
@@ -1,12 +1,19 @@
 package net.pepoirot.bsdiff;
 
 import org.apache.commons.io.FileUtils;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
 
-public final class BsPatchTest extends TestBase {
+import static net.pepoirot.bsdiff.TestUtils.assertContentEquals;
+import static net.pepoirot.bsdiff.TestUtils.getFile;
+
+public final class BsPatchTest {
+
+    private static final String SOURCE = "original.txt";
+    private static final String PATCH = "patch.txt";
+    private static final String PATCHED = "patched.txt";
 
     @Test
     public void testRun() throws IOException {
--- src/test/java/net/pepoirot/bsdiff/TestUtils.java
+++ src/test/java/net/pepoirot/bsdiff/TestUtils.java
@@ -5,22 +5,18 @@ import java.io.IOException;
 import java.net.URL;
 
 import static org.apache.commons.io.FileUtils.readFileToByteArray;
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-abstract class TestBase {
-    protected static final String SOURCE = "original.txt";
-    protected static final String PATCH = "patch.txt";
-    protected static final String PATCHED = "patched.txt";
-
-    protected File getFile(String filename) {
-        final URL resource = this.getClass().getClassLoader().getResource(filename);
+final class TestUtils {
+    static File getFile(String filename) {
+        final URL resource = TestUtils.class.getResource("/" + filename);
         if (resource == null) {
             throw new IllegalStateException("Check " + filename + " is in the test build directory");
         }
         return new File(resource.getFile());
     }
 
-    protected void assertContentEquals(File expected, File actual) throws IOException {
+    static void assertContentEquals(File expected, File actual) throws IOException {
         final byte[] expectedContent = readFileToByteArray(expected);
         final byte[] actualContent = readFileToByteArray(actual);
 
